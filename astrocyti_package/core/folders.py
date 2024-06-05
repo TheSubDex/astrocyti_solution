3d134f05dc01e96cac85772574a97d0ab761f9e6
TheSubDex <74463256+TheSubDex@users.noreply.github.com>
2024-06-06 00:22:28 +0300

    Add files via upload

---
+++ astrocyti_package/core/folders.py
@@ -0,0 +1,39 @@
+def folders_init(Num):
+    if Num == 1:
+        folders = [
+            r'D:\Python\Dataset\last\31_08_2020_tser1\images',
+            r'D:\Python\Dataset\last\31_08_2020_tser1\events'
+        ]
+        return folders
+    elif Num == 2:
+        folders = [
+            r'D:\Python\Dataset\last\31_08_2020_tser2\images',
+            r'D:\Python\Dataset\last\31_08_2020_tser2\events'
+        ]
+        return folders
+    elif Num == 3:
+        folders = [
+            r'D:\Python\Dataset\last\31_08_2020_tser3\images',
+            r'D:\Python\Dataset\last\31_08_2020_tser3\events'
+        ]
+        return folders
+    elif Num == 4:
+        folders = [
+            r'D:\Python\Dataset\last\31_08_2020_tser4\images',
+            r'D:\Python\Dataset\last\31_08_2020_tser4\events'
+        ]
+        return folders
+    elif Num == 5:
+        folders = [
+            r'D:\Python\Dataset\last\2016-05-18_fileNo11_BM3D_z-max\images',
+            r'D:\Python\Dataset\last\2016-05-18_fileNo11_BM3D_z-max\events'
+        ]
+        return folders
+    elif Num == 6:
+        folders= [
+            r'D:\Python\Dataset\last\2016-05-26_fileNo32_BM3D_z-max\images',
+            r'D:\Python\Dataset\last\2016-05-26_fileNo32_BM3D_z-max\events'
+        ]
+        return folders
+    else:
+        return None
